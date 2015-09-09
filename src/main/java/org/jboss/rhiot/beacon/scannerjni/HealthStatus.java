ac533b95fc04e918dd09f12bdfea4b2a323737cd
starksm64 <man$Ir0n>
2015-09-09 15:00:19 -0700

    Handle the four different uptime string formats

--- src/main/java/org/jboss/rhiot/beacon/scannerjni/HealthStatus.java
+++ src/main/java/org/jboss/rhiot/beacon/scannerjni/HealthStatus.java
@@ -103,49 +103,113 @@ public class HealthStatus {
     public static String determineSystemOS() {
         return System.getProperty("os.name");
     }
+    /**
+     * Try expression for less than an hour;
+     * 12:22:35 up 56 min,  5 users,  load average: 0.92, 0.78, 0.71
+     * @param uptime
+     * @return
+     */
+    static boolean parseLessThanHour(String uptime, SystemInfo systemInfo) {
+        Pattern parse = Pattern.compile("up\\s+((\\d+) min?,).*(load average[s]?:.*)");
+        Matcher matcher = parse.matcher(uptime);
+        boolean matched = false;
+        if (matcher.find()) {
+            matched = true;
+            String _minutes = matcher.group(2);
+            String loadAvgs = matcher.group(3);
+
+            long days = 0;
+            long hours = 0;
+            long minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+            long uptimeMS = (minutes * 60 * 1000) + (hours * 3600 * 1000) + (days * 24 * 3600 * 1000);
+            systemInfo.setUptime(uptimeMS);
+            systemInfo.setLoadAverages(loadAvgs);
+        }
+        return matched;
+    }
+
+    /**
+     * Try expression for less than 1 day up;
+     * 14:25:50 up  2:59,  5 users,  load average: 0.21, 0.14, 0.14
+     * @param uptime
+     * @return
+     */
+    static boolean parseLessThanDay(String uptime, SystemInfo systemInfo) {
+        Pattern parse = Pattern.compile("up\\s+(\\d+),\\s+(\\d+):(\\d+),.*(load average[s]?:.*)");
+        Matcher matcher = parse.matcher(uptime);
+        boolean matched = false;
+        if (matcher.find()) {
+            matched = true;
+            String _hours = matcher.group(2);
+            String _minutes = matcher.group(3);
+            String loadAvgs = matcher.group(4);
+
+            long days = 0;
+            long hours = _hours != null ? Integer.parseInt(_hours) : 0;
+            long minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+            long uptimeMS = (minutes * 60 * 1000) + (hours * 3600 * 1000) + (days * 24 * 3600 * 1000);
+            systemInfo.setUptime(uptimeMS);
+            systemInfo.setLoadAverages(loadAvgs);
+        }
+        return matched;
+    }
+
+    /**
+     * 01:25:06 up 1 day,  7:03,  2 users,  load average: 0.09, 0.16, 0.14
+     * @param uptime
+     * @return
+     */
+    static boolean parseMoreThanDay(String uptime, SystemInfo systemInfo) {
+        Pattern parse = Pattern.compile("((\\d+) day[s]?,)?\\s+(\\d+):(\\d+),.*(load average[s]?:.*)");
+        Matcher matcher = parse.matcher(uptime);
+        boolean matched = false;
+        if (matcher.find()) {
+            matched = true;
+            String _days = matcher.group(2);
+            String _hours = matcher.group(3);
+            String _minutes = matcher.group(4);
+            String loadAvgs = matcher.group(5);
+
+            long days = _days != null ? Integer.parseInt(_days) : 0;
+            long hours = _hours != null ? Integer.parseInt(_hours) : 0;
+            long minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
+            long uptimeMS = (minutes * 60 * 1000) + (hours * 3600 * 1000) + (days * 24 * 3600 * 1000);
+            systemInfo.setUptime(uptimeMS);
+            systemInfo.setLoadAverages(loadAvgs);
+        }
+        return matched;
+    }
+
+    static void parse(String uptime, SystemInfo systemInfo) {
+        if(parseMoreThanDay(uptime, systemInfo))
+            return;;
+        if(parseLessThanHour(uptime, systemInfo))
+            return;
+        if(parseLessThanDay(uptime, systemInfo))
+            return;
+    }
+
     public static SystemInfo getSystemInfo() {
-        SystemInfo systemInfo = null;
+        SystemInfo systemInfo = new SystemInfo(0, "0.00 0.00 0.00");
         try {
             Runtime runtime = Runtime.getRuntime();
             Process uptimeProc = runtime.exec("uptime");
             BufferedReader in = new BufferedReader(new InputStreamReader(uptimeProc.getInputStream()));
             String line = in.readLine();
-            long uptimeMS;
-            if (line != null) {
-                // 18:25  up 3 days,  9:01, 12 users, load averages: 1.78 2.10 2.23
-                // 01:25:06 up 1 day,  7:03,  2 users,  load average: 0.09, 0.16, 0.14
-                Pattern parse = Pattern.compile("\\s?((\\d+) day[s]?,)?\\s+(\\d+):(\\d+),.*(load average[s]?:.*)");
-                Matcher matcher = parse.matcher(line);
-                if (matcher.find()) {
-                    String _days = matcher.group(2);
-                    String _hours = matcher.group(3);
-                    String _minutes = matcher.group(4);
-                    String loadAvgs = matcher.group(5);
-
-                    long days = _days != null ? Integer.parseInt(_days) : 0;
-                    long hours = _hours != null ? Integer.parseInt(_hours) : 0;
-                    long minutes = _minutes != null ? Integer.parseInt(_minutes) : 0;
-                    uptimeMS = (minutes * 60*1000) + (hours * 3600 * 1000) + (days * 24*3600*1000);
-
-                    days = uptimeMS / (24*3600*1000);
-                    hours = (uptimeMS - days * 24*3600*1000) / (3600*1000);
-                    minutes = (uptimeMS - days * 24*3600*1000 - hours*3600*1000) / (60*1000);
-                    long seconds = (uptimeMS - days * 24*3600*1000 - hours*3600*1000 - minutes*60*1000) / 1000;
-                    systemInfo = new SystemInfo(uptimeMS, loadAvgs);
-                } else {
-                    log.warn("Failed to parse system uptime");
-                    systemInfo = new SystemInfo(0, "0.00 0.00 0.00");
+            if(!parseMoreThanDay(line, systemInfo)) {
+                if(!parseLessThanDay(line, systemInfo)) {
+                    if(!parseLessThanHour(line, systemInfo)) {
+                        log.warnf("Failed to parse system uptime: %s", line);
+                    }
                 }
             }
-            if(systemInfo != null) {
-                long freeram = runtime.freeMemory();
-                long totalram = runtime.totalMemory();
-                systemInfo.freeram = freeram;
-                systemInfo.totalram = totalram;
-            }
+
+            long freeram = runtime.freeMemory();
+            long totalram = runtime.totalMemory();
+            systemInfo.freeram = freeram;
+            systemInfo.totalram = totalram;
         } catch (IOException e) {
             log.warn("Failed to get uptime", e);
-            systemInfo = new SystemInfo(0, "0.00 0.00 0.00");
         }
         return systemInfo;
     }
